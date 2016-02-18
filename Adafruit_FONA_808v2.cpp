e08fcaa1b9bfacf6c55cd098ab44966d54c19221
Malachi Burke <malachi.burke@gmail.com>
2016-02-18 13:58:48 -0800

    Prep code for IDriver usage

--- Adafruit_FONA_808v2.cpp
+++ Adafruit_FONA_808v2.cpp
@@ -1,6 +1,44 @@
 #include "Adafruit_FONA.h"
 
 #ifdef DRIVERS
+#include "alloca.h"
+
+class SIM808_GNSS_raw : public IGNSS_raw
+{
+  struct Context
+  {
+    char buffer[120];
+    Adafruit_FONA* fona;
+  };
+public:
+  VIRTUAL MetaData getMetaData() OVERRIDE;
+
+  VIRTUAL uint16_t getContextSize() OVERRIDE
+  {
+    return sizeof(Context);
+  }
+
+  VIRTUAL bool getGNSS(void* context, GNSS_raw_ext* data) OVERRIDE;
+};
+
+
+IDriver::MetaData SIM808_GNSS_raw::getMetaData()
+{
+  MetaData md;
+
+  md.deviceType = IDriver::DeviceType::GNSS;
+
+  return md;
+}
+
+bool SIM808_GNSS_raw::getGNSS(void* context, GNSS_raw_ext* data)
+{
+  auto ctx = (Context*) context;
+
+  return false;
+}
+
+
 IDriver::MetaData Adafruit_FONA::getMetaData()
 {
   MetaData md;
@@ -14,7 +52,14 @@ bool Adafruit_FONA::getGNSS(
   double* heading,
   double* altitude)
 {
+  SIM808_GNSS_raw gnssRaw;
+  GNSS_raw_ext data;
+  void* context = alloca(gnssRaw.getContextSize());
+  gnssRaw.initContext(context);
+  gnssRaw.getGNSS(context, &data);
 }
+
+
 #endif
 
 bool Adafruit_FONA_808v2::getGPS(
